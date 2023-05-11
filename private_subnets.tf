2052a1e70fb1f8624c3b4ce2a35de72c635499c4
Aashima Sharma <aashima.sharma@testingxperts.loc>
2023-05-11 18:52:46 +0530

    final

--- private_subnets.tf
+++ private_subnets.tf
@@ -1,8 +1,8 @@
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.ecs_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = "172.31.16.0/20"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "private subnet 1"
@@ -12,8 +12,8 @@ resource "aws_subnet" "private_subnet_1" {
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.ecs_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = "172.31.32.0/20"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "private subnet 2"
--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
 variable "region" {
-  description = "The region the environment is going to be installed into"
+  
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
-
-# VPC variables
-
 variable "vpc_cidr" {
-  description = "CIDR range of VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "172.31.0.0/16"
 }
